477b9887b8cddb792fc4f486e9073738f2ca7648
Pablo De Erauso <pablodeerauso@opendeusto.es>
2022-10-04 13:24:38 +0200

    Añado la clase persona

---
+++ PruebaGit/src/Persona.java
@@ -0,0 +1,18 @@
+
+public class Persona {
+
+	private int edad;
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Persona(int edad) {
+		super();
+		this.edad = edad;
+	}
+}
